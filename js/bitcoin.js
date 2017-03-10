89558f5f6db9c603bda672f096a8cc39840d0d39
camillepechdi <camille.pechdi@gmail.com>
2017-03-10 15:23:57 +0100

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -30,14 +30,31 @@ $(document).ready(function() {
 		success : function(data) {
 			$('#bitcoin_blockchain_number').append(data.blocks);
 		},
-		
-		/*success : function(data) {
-			$('#bitcoin_blockchain_chain').append(data.chain);
-		},*/
 
 		error : function(xhr, status, err) {
 			$('#bitcoin_blockchain_number').append(err+" N/A");
 		}
 	});
 });
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/mininginfo",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_mininginfo').append(data.chain);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_mininginfo').append(err+" N/A");
+		}
+	});
+});
+
+
 
