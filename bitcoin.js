07c8099ea4eafa7586346292bf3e8a17ae58bebf
camillepechdi <camille.pechdi@gmail.com>
2017-03-10 14:18:06 +0100

    Update and rename Appel.js to bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -15,4 +15,22 @@ $(document).ready(function() {
 			$('#bitcoin_block_number').append(err+" N/A");
 		}
 	});
+	
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/block_hash/bitcoin_block_hash/",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_network_hash').append(data.height);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_network_hash').append(err+" N/A");
+		}
+	});
 });
+
